71724c740bf6e29c730bc34c5d1f401ddeba7ba8
PengHaiChen <phcbest2017@outlook.com>
2022-04-14 22:39:32 +0800

    摸了

--- app/src/main/java/org/phcbest/mvvm_demo/viewmodel/ProjectViewModelFactory.kt
+++ app/src/main/java/org/phcbest/mvvm_demo/viewmodel/ProjectViewModelFactory.kt
@@ -4,10 +4,14 @@ import android.util.ArrayMap
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import org.phcbest.mvvm_demo.di.ViewModelSubComponent
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+import java.lang.RuntimeException
 import java.util.concurrent.Callable
 import javax.inject.Inject
 import javax.inject.Singleton
 
+@Suppress("UNCHECKED_CAST")
 @Singleton
 class ProjectViewModelFactory : ViewModelProvider.Factory {
 
@@ -24,7 +28,22 @@ class ProjectViewModelFactory : ViewModelProvider.Factory {
     }
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        TODO("Not yet implemented")
+        var creator = creators[modelClass]
+        if (creator == null) {
+            for (entry in creators.entries) {
+                modelClass.isAssignableFrom(entry.key.javaClass)
+                creator = entry.value
+                break
+            }
+        }
+        if (creator == null) {
+            throw IllegalArgumentException("Unknown model class $modelClass")
+        }
+        try {
+            return creator.call() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
     }
 
 }
